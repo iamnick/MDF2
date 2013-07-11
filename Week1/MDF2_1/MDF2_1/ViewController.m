c63e44519c2ef52ffd923451c4e328ce4a0e032c
Nick Stelzer <iamnick@fullsail.edu>
2013-07-11 16:49:44 -0400

    Week 1 initial upload

---
+++ Week1/MDF2_1/MDF2_1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MDF2_1
+//
+//  Created by Nick Stelzer on 7/11/13.
+//  Copyright (c) 2013 Nick Stelzer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Week1/MDF2_1/MDF2_1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MDF2_1
+//
+//  Created by Nick Stelzer on 7/11/13.
+//  Copyright (c) 2013 Nick Stelzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
